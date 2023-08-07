949351e663ba6c0226780ad1b545432fe45b8a95
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-08-07 10:44:45 +0530

    yaml file

--- 03.var.sh
+++ 03.var.sh
@@ -12,3 +12,11 @@ echo Today date is $DATE
 #Arithmatic Substitution
 ADD=$(( 2+2 ))
 echo ADD of 2+2= $ADD
+
+#environment vairables accesing
+echo Username - $USER
+
+# Access environment variables
+echo Username - $USER
+echo Env Var abc - $abc
+# export abc=100 from CLI can make this variable printed
